bb76fac252db3ccecaa9471ed591cb7bcff3d7c0
HyxiaoGe <18889592303@163.com>
2024-04-11 23:57:21 +0800

    change name to xiaohub

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="welcome">
-    <h1>Welcome to the Application</h1>
+    <h1>Welcome to the XiaoHub</h1>
     <p>Select a feature to get started:</p>
     <div class="nav-buttons">
       <router-link to="/chat" class="nav-button">Chat</router-link>
